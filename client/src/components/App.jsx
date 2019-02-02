99085dfffa46ccbd248fd2b66170eeab6980a239
Chase Kallemeyn <chase.kallemeyn@gmail.com>
2019-02-01 20:54:10 -0600

    complete with step 1

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,1 +1,30 @@
 // This is where most of your work will be done
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      movies: [
+        {title: 'Mean Girls'},
+        {title: 'Hackers'},
+        {title: 'The Grey'},
+        {title: 'Sunshine'},
+        {title: 'Ex Machina'},
+      ]
+    }
+  }
+  render() {
+    const { movies } = this.state;
+    return (
+      <div>
+        <h1>Movie List</h1>
+        <ul>
+          {movies.map((movie) => {
+            return <li> {movie.title}</li>
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
--- client/src/components/Index.jsx
+++ client/src/components/Index.jsx
@@ -1,1 +1,6 @@
 // render your app here in this component
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.jsx'
+
+ReactDOM.render(<App/>, document.getElementById('App'));
